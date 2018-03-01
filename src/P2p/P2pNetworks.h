673192a4d824f069d62476219241b69821aa5ce8
lkihkljh90 <lkihkljh90@web.de>
2018-03-01 20:12:54 +0100

    Initial commit

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,10 @@
+// Copyright (c) 2011-2016 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+  const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0x22, 0x45, 0x87, 0x13, 0x46, 0x3f, 0x2e, 0x71, 0x66, 0x15, 0x72, 0x12, 0x00, 0x53, 0x08, 0x04 } };
+}
